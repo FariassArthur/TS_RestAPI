de5a8ee9a0f9978c692d4aac91d71e903e36b0b5
FariassArthur <arthurfarias.dev@gmail.com>
2024-03-12 22:00:41 -0300

    First Commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+//ENV variables
+require("dotenv").config();
+
+import express from "express";
+import config from "config";
+
+const app = express();
+
+//JSON middleware
+app.use(express.json());
+
+//db
+import db from "../config/db";
+
+//Routes
+import router from "./router";
+
+//Logger
+import Logger from "../config/logger";
+
+app.use("/api", router);
+
+//app port
+const port = config.get<number>("port"); //método get pega os dados
+
+app.listen(port, async () => {
+  await db();
+  Logger.info(`Aplicação está funcionando na porta: ${port}`)
+});
